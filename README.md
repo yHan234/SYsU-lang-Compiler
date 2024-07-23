9a9f083b4790b73f4a787bc3fa1584ff17c8f7f6
YangHan <yhan234@qq.com>
2024-07-23 04:26:35 +0000

    prepare the code for task3

--- README.md
+++ README.md
@@ -18,13 +18,13 @@
 
 ## 笔记
 
-### 为什么选择 SYsU-lang
+### 为什么选择 SYsU-lang？
 
 有详细的[实验文档](https://arcsysu.github.io/SYsU-lang2/)和[本地测试](https://github.com/arcsysu/SYsU-lang2/tree/master/test)，以及[顾宇浩的实验设计手册](https://github.com/arcsysu/SYsU-lang2/tree/master/docs/gyh-manual)也是很好的学习资料。
 
 直接提供了[抽象语义图代码](https://github.com/arcsysu/SYsU-lang2/tree/master/task/2/common)，可以专心于编写 IR 间的转换，而不是各种数据结构的设计。
 
-### 为什么使用 ANTLR
+### 为什么使用 ANTLR？
 
 因为比 flex + bison 易用，并且 ANTLR 官方提供了 [C.g4](https://github.com/antlr/grammars-v4/blob/master/c/C.g4) （但是有 bug ！）。
 
@@ -38,6 +38,6 @@
     
     以支持 `int a[3] = {};` 这种空初始化（Empty initialization）。
 
-### 预处理器在源文件开头添加的 `# 1 "<built-in>" 1` 这类语句是什么
+### 预处理器在源文件开头添加的 `# 1 "<built-in>" 1` 这类语句是什么？
 
 [linemarkers](https://gcc.gnu.org/onlinedocs/cpp/Preprocessor-Output.html)
--- front-end/main.cpp
+++ front-end/main.cpp
@@ -10,9 +10,51 @@
 #include "asg/asg.hpp"
 #include "asg/Typing.hpp"
 #include "asg/Asg2Json.hpp"
+#include "asg/Json2Asg.hpp"
+#include "asg/EmitIR.hpp"
 #include "Ast2Asg.hpp"
 
-int main(int argc, char *argv[])
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/MemoryBuffer.h>
+
+int main_task1(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <input> <output>\n";
+        return -1;
+    }
+
+    std::ifstream inFile(argv[1]);
+    if (!inFile)
+    {
+        std::cout << "Error: unable to open input file: " << argv[1] << '\n';
+        return -2;
+    }
+
+    std::ofstream outFile(argv[2]);
+    if (!outFile)
+    {
+        std::cout << "Error: unable to open output file: " << argv[2] << '\n';
+        return -3;
+    }
+
+    std::cout << "程序 '" << argv[0] << std::endl;
+    std::cout << "输入 '" << argv[1] << std::endl;
+    std::cout << "输出 '" << argv[2] << std::endl;
+
+    antlr4::ANTLRInputStream input(inFile);
+    CLexer lexer(&input);
+
+    antlr4::CommonTokenStream tokens(&lexer);
+    tokens.fill();
+
+    print_tokens_clang(tokens, outFile);
+
+    return 0;
+}
+
+int main_task2(int argc, char *argv[])
 {
     if (argc != 3)
     {
@@ -46,11 +88,6 @@ int main(int argc, char *argv[])
     antlr4::CommonTokenStream tokens(&lexer);
     CParser parser(&tokens);
 
-    // task1: lexer
-    // tokens.fill();
-    // print_tokens_clang(tokens, outFile);
-
-    // task2: parser
     auto ast = parser.compilationUnit();
     Obj::Mgr mgr;
 
@@ -67,4 +104,65 @@ int main(int argc, char *argv[])
     llvm::json::Value json = asg2json(asg);
 
     outFile << json << '\n';
+
+    return 0;
+}
+
+int main_task3(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <input> <output>\n";
+        return -1;
+    }
+
+    auto inFileOrErr = llvm::MemoryBuffer::getFile(argv[1]);
+    if (auto err = inFileOrErr.getError())
+    {
+        std::cout << "Error: unable to open input file: " << argv[1] << '\n';
+        return -2;
+    }
+    auto inFile = std::move(inFileOrErr.get());
+    std::error_code ec;
+    llvm::StringRef outPath(argv[2]);
+    llvm::raw_fd_ostream outFile(outPath, ec);
+    if (ec)
+    {
+        std::cout << "Error: unable to open output file: " << argv[2] << '\n';
+        return -3;
+    }
+
+    auto json = llvm::json::parse(inFile->getBuffer());
+    if (!json)
+    {
+        std::cout << "Error: unable to parse input file: " << argv[1] << '\n';
+        return 1;
+    }
+
+    // 读取 JSON，转换为 ASG
+    Obj::Mgr mgr;
+    Json2Asg json2asg(mgr);
+    auto asg = json2asg(json.get());
+    mgr.mRoot = asg;
+    mgr.gc();
+
+    // 从 ASG 发射到 LLVM IR
+    llvm::LLVMContext ctx;
+    EmitIR emitIR(mgr, ctx);
+    auto &mod = emitIR(asg);
+    mgr.gc();
+
+    // 先把 LLVM IR 写出到文件里，再检查合不合法
+    mod.print(outFile, nullptr, false, true);
+    if (llvm::verifyModule(mod, &llvm::outs()))
+        return 3;
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    // main_task1(argc, argv);
+    // main_task2(argc, argv);
+    main_task3(argc, argv);
 }
